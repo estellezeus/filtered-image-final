9379052efb32f3bde2d87e29e4211e20db80575a
Estelle Z <zeukengestelle@gmail.com>
2022-09-01 17:16:43 +0100

    full code

---
+++ src/server.ts
@@ -0,0 +1,65 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import {filterImageFromURL, deleteLocalFiles} from './util/util';
+
+(async () => {
+
+  // Init the Express application
+  const app = express();
+
+  // Set the network port
+  const port = process.env.PORT || 8082;
+  
+  // Use the body parser middleware for post requests
+  app.use(bodyParser.json());
+
+  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
+  // GET /filteredimage?image_url={{URL}}
+  // endpoint to filter an image from a public url.
+  // IT SHOULD
+  //    1
+  //    1. validate the image_url query
+  //    2. call filterImageFromURL(image_url) to filter the image
+  //    3. send the resulting file in the response
+  //    4. deletes any files on the server on finish of the response
+  // QUERY PARAMATERS
+  //    image_url: URL of a publicly accessible image
+  // RETURNS
+  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+
+  /**************************************************************************** */
+
+  app.get('/filteredimage', async (req, res) => {
+    const image_url = req.query;
+    let myfiles = [];
+
+    if(!image_url){
+      res.status(404).send('Vous devez donner une url valide')
+    } else {
+      let img_str = image_url.image_url;
+      console.log(img_str);
+      let img_filtered = await filterImageFromURL(img_str.toString());
+      res.status(200).sendFile(img_filtered);
+      console.log(img_filtered);
+
+
+      res.on("finish", () => deleteLocalFiles([img_filtered]));
+    }
+
+  });
+
+  //! END @TODO1
+  
+  // Root Endpoint
+  // Displays a simple message to the user
+  app.get( "/", async ( req, res ) => {
+    res.send("try GET /filteredimage?image_url={{}}")
+  } );
+  
+
+  // Start the Server
+  app.listen( port, () => {
+      console.log( `server running http://localhost:${ port }` );
+      console.log( `press CTRL+C to stop server` );
+  } );
+})();
